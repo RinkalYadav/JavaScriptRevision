80bb44816130d835132720480e4904757b341e91
RinkalYadav <rinkalbyadav@gmail.com>
2026-03-19 18:22:42 +0530

    Initial commit

---
+++ day1/script.js
@@ -0,0 +1,23 @@
+const name="Rinkal";
+let age=5;
+let city="Bhadohi";
+
+console.log("My name is "+name);
+console.log("My age is "+age);
+console.log("I am from "+city);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof city);
+
+let arr=[1,2,3,4,5];
+console.log(arr);
+console.log(typeof arr);
+
+let obj={
+    name:"Rinkal",
+    age:5,
+    city:"Bhadohi"
+}
+console.log(obj);
+console.log(typeof obj);
